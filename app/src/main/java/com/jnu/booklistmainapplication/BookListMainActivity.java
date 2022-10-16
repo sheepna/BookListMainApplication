f9ca97b5962c75e3690f535726aa5196769c7f8c
Yangna <yangna1943@outlook.com>
2022-10-16 19:17:30 +0800

    input activity. context menu delete

--- app/src/main/java/com/jnu/booklistmainapplication/BookListMainActivity.java
+++ app/src/main/java/com/jnu/booklistmainapplication/BookListMainActivity.java
@@ -1,26 +1,50 @@
 package com.jnu.booklistmainapplication;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookListMainActivity extends AppCompatActivity {
 
+    public static final int MENU_ID_ADD = 1;
+    public static final int MENU_ID_UPDATE = 2;
+    public static final int MENU_ID_DELETE = 3;
     RecyclerView recyclerView;
     MyAdapater myAdapater;
     List<Book> books = new ArrayList<>();
-
+    ActivityResultLauncher<Intent>addDataLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult()
+    ,result ->{
+        if(null!=result){
+            Intent intent=result.getData();
+            if(result.getResultCode()!=InputBookActivity.RESULT_CODE_SUCCESS){
+                Bundle bundle=intent.getExtras();
+                String title=bundle.getString("title");
+               // double price=bundle.getDouble("price");
+                books.add(1,new Book(title,R.drawable.ic_launcher_background));
+                myAdapater.notifyItemInserted(1);
+            }
+        }
+            } );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +61,43 @@ public class BookListMainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
     }
 
-    class MyAdapater extends RecyclerView.Adapter<MyViewHolder> {
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {//10.16
+        switch(item.getItemId()){
+            case MENU_ID_ADD:
+                Intent intent=new Intent(this,InputBookActivity.class);
+                addDataLauncher.launch(intent);//启动以后，返回，显示消息
+                //startActivity(intent);//启动另外一个界面
+                //Toast.makeText(this,"item add"+item.getOrder()+"clicked",Toast.LENGTH_LONG).show();//获得点击的是那一条数据
+                break;
+            case MENU_ID_UPDATE://toast显示提示信息
+                Toast.makeText(this,"item update"+item.getOrder()+"clicked",Toast.LENGTH_LONG).show();
+                break;
+            case MENU_ID_DELETE:
+
+                AlertDialog alertDialog=new AlertDialog.Builder(this)//删除，要通知调节器去更新界面
+                        .setTitle("Confirmation")
+                                .setMessage("ARE YOU SURE TO DELETE THIS ITEM?")
+                                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                            }
+                                        }).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                books.remove(item.getOrder());
+                                myAdapater.notifyItemRemoved(item.getOrder());
+                            }
+                        }).create();
+                alertDialog.show();
+
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    class MyAdapater extends RecyclerView.Adapter<MyViewHolder> {//重写三个函数
         @NonNull
         @Override
         public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -58,9 +118,11 @@ public class BookListMainActivity extends AppCompatActivity {
         public int getItemCount() {
             return books.size();
         }
+
+
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder {
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
         TextView tv;
         ImageView iv;
 
@@ -68,6 +130,15 @@ public class BookListMainActivity extends AppCompatActivity {
             super(itemView);
             tv = itemView.findViewById(R.id.text_view_book_title);
             iv = itemView.findViewById(R.id.image_view_book_cover);
+
+            itemView.setOnCreateContextMenuListener(this);//10.16
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {//10.16
+            contextMenu.add(0,MENU_ID_ADD,getAdapterPosition(),"Add"+getAdapterPosition());//获得当前点击的项目的位置
+            contextMenu.add(0,MENU_ID_UPDATE,getAdapterPosition(),"Update"+getAdapterPosition());
+            contextMenu.add(0,MENU_ID_DELETE,getAdapterPosition(),"Delete"+getAdapterPosition());
         }
     }
 
@@ -79,11 +150,16 @@ public class BookListMainActivity extends AppCompatActivity {
 }
 class Book{
     public String title;
+    //public Double price;
     public int id;
     public Book(String s, int i) {
         this.title=s;
+        //this.price=p;
         this.id=i;
     }
+
+
+
     public String getTitle(){
         return this.title;
     }
