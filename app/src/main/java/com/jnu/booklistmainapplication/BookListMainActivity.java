b34706a399e55a7ea384ab27ac6ef9734f9d1686
Yangna <yangna1943@outlook.com>
2022-11-02 11:18:20 +0800

    finish contextMenu Add Delete

--- app/src/main/java/com/jnu/booklistmainapplication/BookListMainActivity.java
+++ app/src/main/java/com/jnu/booklistmainapplication/BookListMainActivity.java
@@ -1,13 +1,16 @@
 package com.jnu.booklistmainapplication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -21,7 +24,8 @@ public class BookListMainActivity extends AppCompatActivity {
 
     private ArrayList<Book>books;
     private MainRecycleViewAdapter mainRecycleViewAdapter;
-
+    public static final int MENU_ID_ADD = 1;
+    public static final int MENU_ID_DELETE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +44,40 @@ public class BookListMainActivity extends AppCompatActivity {
 
         mainRecycleViewAdapter= new MainRecycleViewAdapter(books);
         recyclerViewMain.setAdapter(mainRecycleViewAdapter);
+
     }
+
+    //在Activity中重载onContextItemSelected
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case MENU_ID_ADD:
+                books.add(item.getOrder(),new Book("added"+item.getOrder(),R.drawable.ic_launcher_background));
+                mainRecycleViewAdapter.notifyItemChanged(item.getOrder());
+                break;
+            case MENU_ID_DELETE:
+                AlertDialog alertDialog;
+                alertDialog=new AlertDialog.Builder(this)
+                        .setTitle(R.string.confirmation)
+                        .setMessage(R.string.sure_to_delete)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                books.remove(item.getOrder());
+                                mainRecycleViewAdapter.notifyItemRemoved(item.getOrder());
+                            }
+                        }).setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).create();
+                alertDialog.show();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
     public class MainRecycleViewAdapter extends RecyclerView.Adapter<MainRecycleViewAdapter.ViewHolder>{
 
         private ArrayList<Book>localDataSet;
@@ -55,7 +92,7 @@ public class BookListMainActivity extends AppCompatActivity {
                 textViewTitle=itemView.findViewById(R.id.text_view_book_title);
                 imageViewImage=itemView.findViewById(R.id.image_view_book_cover);
 
-                itemView.setOnCreateContextMenuListener(this);
+                itemView.setOnCreateContextMenuListener(this);//在ViewHolder实现onContextMenuListener接口
             }
 
             public TextView getTextViewTitle() {
@@ -68,7 +105,8 @@ public class BookListMainActivity extends AppCompatActivity {
 
             @Override
             public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
-
+                contextMenu.add(0,MENU_ID_ADD,getAdapterPosition(),"Add "+getAdapterPosition());
+                contextMenu.add(0,MENU_ID_DELETE,getAdapterPosition(),"Delete "+getAdapterPosition());
             }
         }
 
